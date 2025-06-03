d0a07dd9807f1f33e19ea166bdefa708e9b45be5
sanvik223 <akramytbd401@gmail.com>
2025-06-03 14:26:23 +0200

    Create admob.js

---
+++ app/config/admob.js
@@ -0,0 +1,6 @@
+export const AdMobConfig = {
+  appId: 'ca-app-pub-3510277807559737~4821936439',
+  banner: 'ca-app-pub-3510277807559737/2059223482',
+  interstitial: 'ca-app-pub-3510277807559737/8588138664',
+  rewarded: 'ca-app-pub-3510277807559737/9882691428',
+};
